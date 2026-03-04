11ecd479691622f4e0f2749bd86f614d3c279451
truongvd05 <truongvdf8@fullstack.edu.vn>
2026-03-04 10:09:18 +0700

    Updates

---
+++ assets/index-B8AMH8nY.js
@@ -0,0 +1,9 @@
+import{j as e,m as s,D as n}from"./index-_LIdIUbq.js";function o({className:r,...t}){return e.jsx("textarea",{"data-slot":"textarea",className:s("border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",r),...t})}function l({message:r,right:t,user:a}){const{theme:i}=n();return e.jsxs(e.Fragment,{children:[e.jsx("div",{className:`
+            ${t?"flex justify-end ml-auto":"flex"} 
+            w-[80%]
+            `,children:e.jsx("p",{className:`
+                    break-all whitespace-pre-wrap
+                    ${a&&i==="light"?"bg-olive-100 ":""}
+                    ${t?"border border-white":""}
+                    px-2 py-3 rounded-2xl
+                    `,children:r.content})}),r.status==="sending"&&e.jsx("span",{className:"text-xs text-gray-400 ml-2",children:"Đang gửi..."})]})}export{l as M,o as T};
