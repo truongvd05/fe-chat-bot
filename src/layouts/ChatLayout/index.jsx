ef8a25484a20e95c4486e01dfbfd37a70035f31a
truongvd05 <truongvdf8@fullstack.edu.vn>
2026-03-18 15:18:37 +0700

    fix n css

--- src/layouts/ChatLayout/index.jsx
+++ src/layouts/ChatLayout/index.jsx
@@ -11,12 +11,12 @@ function ChatLayout(){
         return <Navigate to="/login" replace/>
     }
     return (
-    <div className="min-h-screen overflow-x-hidden">
+    <div className="overflow-x-hidden">
         <div className="fixed top-0 bottom-0 w-70 z-10">
             <Sidebar/>
         </div>
 
-        <div className="h-full flex-1 flex flex-col md:mr-25 md:ml-25 lg:ml-70  lg:mr-37.5 xl:mr-70 min-w-0 border-r border-l pb-30 ">
+        <div className="min-h-screen border-r border-l flex-1 flex flex-col md:mr-25 md:ml-25 lg:ml-70  lg:mr-37.5 xl:mr-70 min-w-0  pb-30 ">
             <Outlet/>
         </div>
 
